57c1e14868185d9a6c8823a25656e059f2521a2b
JAINE JOSE DA SILVA <48960882+JodaStozo@users.noreply.github.com>
2022-02-10 09:37:42 -0300

    Add files via upload

---
+++ Joda.JSON.Testador/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace Joda.JSON.Testador
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonProcessar = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.buttonPesquisar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonProcessar
+            // 
+            this.buttonProcessar.Location = new System.Drawing.Point(313, 12);
+            this.buttonProcessar.Name = "buttonProcessar";
+            this.buttonProcessar.Size = new System.Drawing.Size(75, 23);
+            this.buttonProcessar.TabIndex = 0;
+            this.buttonProcessar.Text = ">>";
+            this.buttonProcessar.UseVisualStyleBackColor = true;
+            this.buttonProcessar.Click += new System.EventHandler(this.buttonProcessar_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 12);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(307, 426);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(394, 297);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox2.Size = new System.Drawing.Size(307, 103);
+            this.textBox2.TabIndex = 1;
+            // 
+            // treeView1
+            // 
+            this.treeView1.Location = new System.Drawing.Point(394, 12);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(307, 279);
+            this.treeView1.TabIndex = 2;
+            this.treeView1.AfterExpand += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterExpand);
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.treeView1_AfterSelect);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(394, 418);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(262, 20);
+            this.textBox3.TabIndex = 3;
+            // 
+            // buttonPesquisar
+            // 
+            this.buttonPesquisar.Location = new System.Drawing.Point(662, 416);
+            this.buttonPesquisar.Name = "buttonPesquisar";
+            this.buttonPesquisar.Size = new System.Drawing.Size(39, 23);
+            this.buttonPesquisar.TabIndex = 4;
+            this.buttonPesquisar.Text = "Pesq";
+            this.buttonPesquisar.UseVisualStyleBackColor = true;
+            this.buttonPesquisar.Click += new System.EventHandler(this.buttonPesquisar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonPesquisar);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.treeView1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.buttonProcessar);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonProcessar;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TreeView treeView1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button buttonPesquisar;
+    }
+}
+
---
+++ Joda.JSON.Testador/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Joda.JSON.Testador
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Arvore minhArvore;
+        private void buttonProcessar_Click(object sender, EventArgs e)
+        {
+            Joda.JSON.ParObj meuPar = new Joda.JSON.ParObj();
+            meuPar.Nome = "Json";
+            meuPar.Deserealizar(this.textBox1.Text);
+            this.textBox2.Text = string.Empty;
+            this.treeView1.Nodes.Clear();
+            foreach (JSON.ParBase valorLido in meuPar.Valor)
+            {
+                this.textBox2.Text += "\r\n";
+                this.textBox2.Text += string.Format("{0} = {1}", valorLido.Nome, valorLido.Valor);
+
+                TreeNode meuNo = new TreeNode();
+                meuNo.Text = valorLido.Nome;
+                meuNo.Tag = valorLido;
+                this.treeView1.Nodes.Add(meuNo);
+                povoar(meuNo);
+            }
+            minhArvore = new JSON.Arvore(this.textBox1.Text);
+
+            this.textBox2.Text += "Arvore de dados povoada com sucesso";
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            Joda.JSON.ParBase meuTag = (Joda.JSON.ParBase)e.Node.Tag;
+            this.textBox2.Text = meuTag.Valor;
+        }
+        private void povoar(TreeNode meuNo)
+        {
+            Joda.JSON.ParBase meuTag = (Joda.JSON.ParBase)meuNo.Tag;
+            String s = meuTag.Valor.Trim();
+            if (s.StartsWith("{"))
+            {
+                Joda.JSON.ParObj meuPar = new Joda.JSON.ParObj();
+                meuPar.Nome = meuTag.Nome;
+                meuPar.Deserealizar(s);
+                meuNo.Nodes.Clear();
+                foreach (JSON.ParBase valorLido in meuPar.Valor)
+                {
+                    TreeNode novoNo = new TreeNode();
+                    novoNo.Text = valorLido.Nome;
+                    novoNo.Tag = valorLido;
+                    meuNo.Nodes.Add(novoNo);
+                }
+            }
+            if (s.StartsWith("["))
+            {
+                Joda.JSON.ParArr meuPar = new Joda.JSON.ParArr();
+                meuPar.Nome = meuTag.Nome;
+                meuPar.Deserealizar(s);
+                meuNo.Nodes.Clear();
+                foreach (Joda.JSON.ParBase valorLido in meuPar.Valor)
+                {
+                    TreeNode novoNo = new TreeNode();
+                    novoNo.Text = valorLido.Nome;
+                    novoNo.Tag = valorLido;
+                    meuNo.Nodes.Add(novoNo);
+                }
+            }
+            if (s.StartsWith("\""))
+            {
+                Joda.JSON.ParStr meuPar = new Joda.JSON.ParStr();
+                meuPar.Nome = meuTag.Nome;
+                meuPar.Deserealizar(s);
+                meuNo.Tag = meuPar;
+            }
+        }
+
+        private void treeView1_AfterExpand(object sender, TreeViewEventArgs e)
+        {
+            foreach(TreeNode noLindo in e.Node.Nodes)
+            {
+                povoar(noLindo);
+            }
+        }
+
+        private void buttonPesquisar_Click(object sender, EventArgs e)
+        {
+            JSON.ParBase achei = minhArvore.Pesquisar(textBox3.Text);
+            if (achei != null)
+            {
+                this.textBox2.Text = achei.Valor;
+            } else
+            {
+                this.textBox2.Text = "Caminho não localizado!";
+            }
+        }
+    }
+}
